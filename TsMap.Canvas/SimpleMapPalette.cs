e55b33d82ab99c6295edfd4ab65c0be050a8bbc0
Dat Tunes <166637092+DrFlaschenbier@users.noreply.github.com>
2026-09-24 20:20:36 +0200

    Add NTG dark map palette

--- TsMap.Canvas/SimpleMapPalette.cs
+++ TsMap.Canvas/SimpleMapPalette.cs
@@ -1,23 +1,24 @@
-﻿using System.Drawing;
+using System.Drawing;
 
 namespace TsMap.Canvas
 {
+    /// <summary>
+    /// Nordic Transport Group dark map theme.
+    /// NTG CI: ice blue #7BBDE9, white #FFFFFF, rock grey #6E747A.
+    /// </summary>
     public class SimpleMapPalette : MapPalette
     {
         public SimpleMapPalette()
         {
-            Background = new SolidBrush(Color.FromArgb(72, 78, 102));
-            Road = Brushes.White;
-            PrefabRoad = Brushes.White;
-            PrefabLight = new SolidBrush(Color.FromArgb(236, 203, 153));
-            PrefabDark = new SolidBrush(Color.FromArgb(225, 163, 56));
-            PrefabGreen = new SolidBrush(Color.FromArgb(170, 203, 150)); // TODO: Check if green has a specific z-index
-
-            CityName = Brushes.LightCoral;
-
-            FerryLines = new SolidBrush(Color.FromArgb(80, 255, 255, 255));
-
-            Error = Brushes.LightCoral;
+            Background = new SolidBrush(Color.FromArgb(17, 24, 32));       // #111820
+            Road = new SolidBrush(Color.FromArgb(232, 237, 242));          // #E8EDF2
+            PrefabRoad = new SolidBrush(Color.FromArgb(232, 237, 242));
+            PrefabLight = new SolidBrush(Color.FromArgb(72, 82, 91));      // dark urban
+            PrefabDark = new SolidBrush(Color.FromArgb(43, 51, 59));
+            PrefabGreen = new SolidBrush(Color.FromArgb(43, 66, 61));
+            CityName = Brushes.White;
+            FerryLines = new SolidBrush(Color.FromArgb(123, 189, 233));    // NTG ice blue
+            Error = new SolidBrush(Color.FromArgb(255, 100, 100));
         }
     }
 }
